dc23baa14c8dc43b1fbd711b5834d0ed6cb4cee0
Tushar Lohar <TUSHAR.LOHAR@svkmmumbai.onmicrosoft.com>
2024-05-04 12:16:41 +0530

    commit insta card

---
+++ script.js
@@ -0,0 +1,17 @@
+var istatus = document.querySelector("h5") 
+var addFriend = document.querySelector("#add")
+var check = 0
+
+addFriend.addEventListener("click",function(){
+    if(check == 0) {
+        istatus.innerHTML = "Friends"
+        istatus.style.color = "green"
+        check = 1
+    }
+    else{
+        istatus.innerHTML = "Stranger"
+        istatus.style.color = "red"
+        check = 0
+
+    }
+})
